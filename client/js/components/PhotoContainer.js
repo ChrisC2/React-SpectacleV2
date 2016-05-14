c9dafe7ea380065909925b0310f32530e440177a
ChrisC2 <chrisjc790@gmail.com>
2016-05-13 21:32:34 -0700

    add whatwg-fetch for safari

--- client/js/components/PhotoContainer.js
+++ client/js/components/PhotoContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
-import Photo from "./Photo"
+import Photo from "./Photo";
+import 'whatwg-fetch';
 
 export default class PhotoContainer extends React.Component {
   constructor(props) {
